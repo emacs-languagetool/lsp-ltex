98435127e24719a398c41ebaf14d26928de4632a
JenChieh <jcs090218@gmail.com>
2025-01-01 00:51:53 -0800

    Update copyright year

--- lsp-ltex.el
+++ lsp-ltex.el
@@ -1,6 +1,6 @@
 ;;; lsp-ltex.el --- LSP Clients for LTEX  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021-2024  Shen, Jen-Chieh
+;; Copyright (C) 2021-2025  Shen, Jen-Chieh
 ;; Created date 2021-04-03 00:35:56
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
